6b41c19585c51a58fa3b87a6e56c84a4d6e91b7e
OMAR SALAZAR PEREZ <omar.salazar@elektra.com.mx>
2023-02-24 09:31:42 -0600

    Primer commit estructura de proyecto

---
+++ src/main/java/com/superapp/notificaciones/tokenadmin/TokenadminApplication.java
@@ -0,0 +1,13 @@
+package com.superapp.notificaciones.tokenadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenadminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/superapp/notificaciones/tokenadmin/TokenadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superapp.notificaciones.tokenadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
